e34e1c89af91710013f1e0b519e7ddd1f07070c3
Jonathan Abreu <abreu.jonathan220@gmail.com>
2018-02-20 12:20:58 -0300

    Calculadora - op: soma

--- calculadora.c
+++ calculadora.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 //17/02/2018
 //Autor Jonathan Abreu
@@ -10,17 +11,37 @@ void menu(float x){
 	printf("\n2 - Subtração");
 	printf("\n3 - Multiplicação");
 	printf("\n4 - Divisão");
-	printf("\n5 - Sair");
+	printf("\n5 - Limpar resultado");
+	printf("\n6 - Sair");
+}
+
+float soma(float s){
+	
+	float total = s;
+	float entr = 1;
+
+	while(entr >= 1){
+		printf("Para encerrar digite um número negativo: \n");
+		printf("Insira o valor:");
+		scanf("%f", &entr);
+		if(entr>=0)
+			total += entr;
+	}
+
+	return total;
 }
 
 int main(int argc, char const *argv[])
 {
+
 	float entrada = 0;
-	int condicao = 1;
-	while(condicao){
+	int condicao = 0;
+	
+	while(condicao >= 0){
 		menu(entrada);
 		printf("\nOpção: ");
 		scanf("%d", &condicao);
+
 		//Toda opção deverá retornar um valor para 
 		//'entrada'
 		//entrada só é renicializada quando o programa
@@ -29,14 +50,18 @@ int main(int argc, char const *argv[])
 		// e no default so switch
 		switch (condicao){
 			case 1:
-				entrada = 2;
+				entrada = soma(entrada);
 				break;
 			case 5:
-				condicao = 0;
+				entrada = 0;
+				break;
+			case 6:
+				condicao = -1;
 				break;
 			default:
 				printf("Opção incorreta!!!\n");
-		}//fim so switch
+		}//fim do switch
+	
 	}//fim do while
 	return 0;
 }//fim da função main
